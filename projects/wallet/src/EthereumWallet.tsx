bf5aa935232fc2103c8cd02d4a08ffbc2d0e4c2e
AbhishekBharati <210305105048@paruluniversity.ac.in>
2024-08-18 22:28:21 +0530

    Wrote the logic to add Ethereum addresses

--- projects/wallet/src/EthereumWallet.tsx
+++ projects/wallet/src/EthereumWallet.tsx
@@ -1,10 +1,31 @@
+import { useState } from "react"
+import { mnemonicToSeed } from "bip39"
+import { Wallet, HDNodeWallet } from "ethers"
+
 interface EthWalletParams {
   mnemonic: string
 }
 export const EthereumWallet = ({ mnemonic }: EthWalletParams) => {
+  const [currentIndex, setCurrentIndex] = useState<number>(0)
+  const [addresses, setAddresses] = useState<string[]>([]);
+
+  const handleAddEthereum = async () => {
+    const seed: Buffer = await mnemonicToSeed(mnemonic);
+    const derivationPath: string = `m/44'/60'/${currentIndex}'/0'`;
+    const hdNode = HDNodeWallet.fromSeed(seed);
+    const child = hdNode.derivePath(derivationPath);
+    const privateKey = child.privateKey;
+    const wallet = new Wallet(privateKey);
+    setCurrentIndex(currentIndex + 1);
+    setAddresses([...addresses, wallet.address]);
+  }
+
   return (
     <div>
-      <h1>Ethereums... World</h1>
+      <button onClick={handleAddEthereum}>
+        Add Ethereum Wallet
+      </button>
+      <b>{addresses.map(p => <div>{p}</div>)}</b>
     </div>
   )
 }
